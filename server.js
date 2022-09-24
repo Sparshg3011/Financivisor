57a827aa0c9bc3ae0e4769f99788008c3d20a2e4
sparsh gupta <90498262+Sparshg3011@users.noreply.github.com>
2022-09-24 18:23:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const dbConnect = require('./dbConnect');
 const app = express();
 app.use(express.json());
+const path = require('path');
 const userRoute = require('./routes/usersRoute')
 const transactionRoute = require('./routes/transactionsRoute');
-
 app.use('/api/users/', userRoute);
 app.use('/api/transactions/', transactionRoute);
 
-const port = 8000;
+const port =process.env.PORT || 8000;
 
-app.get('/', (req, res) => res.send("Hello World"));
+if(process.env.NODE_ENV === 'production')
+{
+    app.use('/', express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+    })
+}
+
 app.listen(port, () => console.log(`App listening on port ${port}.`));
